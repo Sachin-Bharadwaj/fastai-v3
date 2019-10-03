651cec5abcd633c54b61771cf5e33e85d1e609a9
sachin <sachinb@steradiansemi.com>
2019-10-03 06:49:20 -0700

    changed urls and class labels

--- app/server.py
+++ app/server.py
@@ -9,10 +9,24 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1BvAJkHUtxcGzKpjZ7Cl8THyPLJ24G5kP'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['dholak',
+ 'ektara',
+ 'esraj',
+ 'flute',
+ 'ghatam',
+ 'harmonium',
+ 'mridangam',
+ 'santoor',
+ 'sarangi',
+ 'sarod',
+ 'shehnai',
+ 'sitar',
+ 'tabla',
+ 'tambura',
+ 'veena']
 path = Path(__file__).parent
 
 app = Starlette()
